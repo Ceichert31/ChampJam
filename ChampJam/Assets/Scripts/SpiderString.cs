7d923840add489c62e902187a5bddc37c0e6c487
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-11-08 18:08:38 -0500

    feat: Implemented tutorial in main

--- ChampJam/Assets/Scripts/SpiderString.cs
+++ ChampJam/Assets/Scripts/SpiderString.cs
@@ -32,7 +32,7 @@ public class SpiderString : MonoBehaviour
         {
             spider.GetComponent<SpiderAI>().enabled = true;
             spiderStringChild.parent = null;
-            spiderStringChild.DOMoveY(10, 2f).SetEase(Ease.InBack);
+            spiderStringChild.DOMoveY(10, 2f).SetEase(Ease.InOutElastic);
             hasBeenDeployed = true;
         });
     }
