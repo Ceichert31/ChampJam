407029a987387d314bdd3838e8cbb83ac41746df
Matthew <matthew.oneill@mymail.champlain.edu>
2025-11-08 12:31:12 -0500

    scrolling bg

--- ChampJam/Assets/Scripts/ScrollUV.cs
+++ ChampJam/Assets/Scripts/ScrollUV.cs
@@ -2,21 +2,57 @@ using UnityEngine;
 
 public class ScrollUV : MonoBehaviour
 {
+    enum ScrollType { LINEAR, SINE, SHIFT, BOUNCE /*unimplemented*/ }
+
+    [SerializeField] ScrollType scrollTypeX = ScrollType.LINEAR;
+    [SerializeField] ScrollType scrollTypeY = ScrollType.LINEAR;
     [SerializeField] float scrollSpeedX = 1.0f;
     [SerializeField] float scrollSpeedY = 1.0f;
 
-    [SerializeField] Material material;
+    Material material;
 
     void Start()
     {
-        //material = GetComponent<MeshRenderer>().material;
+        material = GetComponent<MeshRenderer>().material;
     }
 
     void Update()
     {
-        float offsetX = scrollSpeedX * Time.deltaTime;
-        float offsetY = scrollSpeedY * Time.deltaTime;
+        material.mainTextureOffset += GetOffset();
+    }
 
-        material.mainTextureOffset += new Vector2(offsetX, offsetY);
+    Vector2 GetOffset()
+    {
+        return new Vector2(GetScroll(scrollTypeX, scrollSpeedX), GetScroll(scrollTypeY, scrollSpeedY));
+    }
+
+    float GetScroll(ScrollType scrollType, float scrollSpeed)
+    {
+        switch (scrollType)
+        {
+            case ScrollType.LINEAR:
+                return LinearScroll(scrollSpeed);
+            case ScrollType.SINE:
+                return SineScroll(scrollSpeed);
+            case ScrollType.SHIFT:
+                return ShiftScroll(scrollSpeed);
+            default:
+                return 0;
+        }
+    }
+
+    float LinearScroll(float scrollSpeed)
+    {
+        return scrollSpeed * Time.deltaTime;
+    }
+
+    float SineScroll(float scrollSpeed)
+    {
+        return scrollSpeedX * Mathf.Sin(Time.time) * Time.deltaTime;
+    }
+
+    float ShiftScroll(float scrollSpeed)
+    {
+        return scrollSpeedX * Mathf.Abs(Mathf.Sin(Time.time)) * Time.deltaTime;
     }
 }
