69729541e3c0b35c7f742c94dd9e6f82d08fdda5
Matthew <matthew.oneill@mymail.champlain.edu>
2025-11-08 14:44:50 -0500

    dynamic music and background update

--- ChampJam/Assets/Scripts/MusicManager.cs
+++ ChampJam/Assets/Scripts/MusicManager.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private AudioSource baseMusic;
+    [SerializeField] private AudioSource mothMusic;
+    [SerializeField] private AudioSource fleaMusic;
+
     void Start()
     {
-        
+        Activate();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetActiveBugs();
+    }
+
+    void Activate()
+    {
+        baseMusic.Play();
+        mothMusic.Play();
+        fleaMusic.Play();
+    }
+    
+    void GetActiveBugs()
+    {
+        if (GameManager.Instance.HasMothOnScreen())
+        {
+            mothMusic.volume = 1;
+        }
+        else
+        {
+            mothMusic.volume = 0;
+        }
+
+        if (GameManager.Instance.HasFlyOnScreen())
+        {
+            fleaMusic.volume = 1;
+        }
+        else
+        {
+            fleaMusic.volume = 0;
+        }
     }
 }
