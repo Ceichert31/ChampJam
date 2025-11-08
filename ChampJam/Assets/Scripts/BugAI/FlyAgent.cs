fd3a935c03d9a3bb3e3aa6a20c3dda54e1bb03a0
brody-brody <brody.silva@mymail.champlain.edu>
2025-11-08 10:49:05 -0500

    bugs moving with lantern

--- ChampJam/Assets/Scripts/BugAI/FlyAgent.cs
+++ ChampJam/Assets/Scripts/BugAI/FlyAgent.cs
@@ -1,23 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
-
 public class FlyAgent : MonoBehaviour
 {
+    [System.Serializable]
+    public enum BugType
+    {
+        MOTH,
+        FLY,
+        DEFAULT
+    }
+
+    [Header("Fly Data")]
+    // improtant
+    public BugType bugType = BugType.DEFAULT;
+    public bool inRadius = false;
+    public bool litWhenInRadius = false;
+    public Transform latestLightPos;
+    // end of important
+
+    [Header("Fly Settings")]
+
     [SerializeField]
     private float agentSpeed = 1.0f;
-
     [SerializeField]
     private float fleeDistance = 0.5f;
-
     [SerializeField]
-    private float targetChangeTimer = 5f;
 
+    private float targetChangeTimer = 5f;
     private float changeTimer;
 
     private IPathfinder pathfinder;
-
     private Rigidbody2D rb;
-
     private Vector2 target;
 
     private Vector3 SpiderPos => GameManager.Instance.SpiderPos.position;
@@ -25,28 +38,81 @@ public class FlyAgent : MonoBehaviour
     private void Start()
     {
         pathfinder = GetComponent<IPathfinder>();
-
         rb = GetComponent<Rigidbody2D>();
-
         target = GameManager.Instance.GetRandomPointInBounds();
 
         changeTimer = Time.time + targetChangeTimer;
+
+        // temporary 50/50 for bug decisions
+        if (Random.value > 0.5f)
+        {
+            bugType = BugType.MOTH;
+        }
+        else
+        {
+            bugType = BugType.FLY;
+        }
     }
 
     private void Update()
     {
         CalculateTarget();
+        Debug.Log("inRadius: " + inRadius);
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = pathfinder.GetPathVelocity((target - new Vector2(transform.position.x, transform.position.y)).normalized) * agentSpeed;
-        transform.up = Vector2.Lerp(transform.up, rb.linearVelocity, Time.deltaTime);
+        PerformPathing();
     }
 
-    void CalculateTarget()
+    private void PerformPathing()
     {
-        if (Vector3.Distance(transform.position, target) < 0.1f || changeTimer < Time.time)
+        if (!inRadius)
+        {
+            Debug.Log("normal pathfding");
+            rb.linearVelocity = pathfinder.GetPathVelocity((target - new Vector2(transform.position.x, transform.position.y)).normalized) * agentSpeed;
+            transform.up = Vector2.Lerp(transform.up, rb.linearVelocity, Time.deltaTime);
+            //end early
+            return;
+        }
+
+        switch (bugType)
+        {
+            case BugType.MOTH:
+                MothLightPathing();
+                Debug.Log("moth pathfding");
+                break;
+
+            case BugType.FLY:
+                FlyLightPathing();
+                Debug.Log("fly pathfding");
+                break;
+
+            case BugType.DEFAULT:
+                Debug.Log("this shouldn't happen");
+                break;
+        }
+    }
+
+    private void MothLightPathing()
+    {
+        // move towards light
+        Vector2 toLightDir = ((Vector2)latestLightPos.position - (Vector2)transform.position).normalized;
+        rb.linearVelocity = pathfinder.GetPathVelocity(toLightDir) * agentSpeed;
+        transform.up = Vector2.Lerp(transform.up, rb.linearVelocity, Time.fixedDeltaTime * 3f);
+    }
+
+    private void FlyLightPathing()
+    {
+        // flee from light
+        Vector2 awayFromLightDir = ((Vector2)transform.position - (Vector2)latestLightPos.position).normalized;
+        rb.linearVelocity = pathfinder.GetPathVelocity(awayFromLightDir) * agentSpeed;
+        transform.up = Vector2.Lerp(transform.up, rb.linearVelocity, Time.fixedDeltaTime * 3f);
+    }
+
+    private void CalculateTarget()
+    {
+        if (Vector2.Distance(transform.position, target) < 0.1f || changeTimer < Time.time)
         {
             changeTimer = Time.time + targetChangeTimer;
             target = GameManager.Instance.GetRandomPointInBounds();
