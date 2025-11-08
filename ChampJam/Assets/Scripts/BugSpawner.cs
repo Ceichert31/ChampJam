fb1c4caef0ae8e1fceec2e5f4e6b4991b1af7af0
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-11-07 22:35:21 -0500

    feat: Implemented enemy spawning

--- ChampJam/Assets/Scripts/BugSpawner.cs
+++ ChampJam/Assets/Scripts/BugSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BugSpawner : MonoBehaviour
@@ -5,8 +6,38 @@ public class BugSpawner : MonoBehaviour
     [SerializeField]
     private AnimationCurve spawnRate;
 
+    [SerializeField]
+    private List<GameObject> spawnList;
+
+    [SerializeField]
+    private Transform bugParent;
+
+    private float spawnTime;
+
+    private void Start()
+    {
+        spawnTime = spawnRate.Evaluate(Time.time) + Time.time;
+    }
+
     public void Update()
     {
-        
+        if (spawnTime < Time.time)
+        {
+            spawnTime = spawnRate.Evaluate(Time.time) + Time.time;
+            SpawnBugs();
+        }
+    }
+
+    private void SpawnBugs()
+    {
+        int bugNum = Random.Range(0, 3);
+
+        for (int i = 0; i < bugNum; i++)
+        {
+            //Spawn
+            int spawnIndex = Random.Range(0, spawnList.Count - 1);
+
+            Instantiate(spawnList[spawnIndex], bugParent);
+        }
     }
 }
