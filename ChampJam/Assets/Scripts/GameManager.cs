0277c2c73e9eac2ee2ccbea7ed513729049ed06f
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-11-07 20:56:30 -0500

    feat: Bugs that hit the goal get deleted and add score

--- ChampJam/Assets/Scripts/GameManager.cs
+++ ChampJam/Assets/Scripts/GameManager.cs
@@ -5,8 +5,15 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public Transform bugGoal;
+
+    private int score = 0;
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
     }
 }
