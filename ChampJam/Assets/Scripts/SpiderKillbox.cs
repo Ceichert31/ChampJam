c3c8f3f61de278d939a335d742eb8528b0325c7a
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-11-08 12:05:33 -0500

    feat: Added bug eat dotween

--- ChampJam/Assets/Scripts/SpiderKillbox.cs
+++ ChampJam/Assets/Scripts/SpiderKillbox.cs
@@ -1,13 +1,21 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SpiderKillbox : MonoBehaviour, IDestroy
 {
+    [SerializeField]
+    private Transform eatPoint;
+
     public void DestroyBug(GameObject bug)
     {
-        //Add points
-        GameManager.Instance.AddScore(-100);
-        GameManager.Instance.RemoveBug(bug);
-        Destroy(bug);
+        bug.transform.DOMove(eatPoint.position, 0.3f).OnComplete(() => {  //Add points
+
+            //minus points text
+            GameManager.Instance.AddScore(-100);
+            GameManager.Instance.RemoveBug(bug);
+            Destroy(bug);
+        });
+        
     }
 }
 
