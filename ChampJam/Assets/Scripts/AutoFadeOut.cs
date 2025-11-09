7df5248b13b59e13bfba0e5f567878b3a8817781
Matthew <matthew.oneill@mymail.champlain.edu>
2025-11-08 20:15:18 -0500

    delayed auto fadeout

--- ChampJam/Assets/Scripts/AutoFadeOut.cs
+++ ChampJam/Assets/Scripts/AutoFadeOut.cs
@@ -6,6 +6,11 @@ public class AutoFadeOut : MonoBehaviour
 {
     void Start()
     {
+        Invoke(nameof(DoThing), 0.1f);
+    }
+
+    void DoThing()
+    {
         GetComponent<Image>().DOColor(Color.clear, 1);
     }
 }
